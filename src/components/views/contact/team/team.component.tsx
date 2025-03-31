b6052b0ab7f4aa8098565575d1262755b6ede851
Michał Kowalski <michalkowit@gmail.com>
2025-03-31 21:02:33 +0200

    finished contact page

--- src/components/views/contact/team/team.component.tsx
+++ src/components/views/contact/team/team.component.tsx
@@ -1,5 +1,4 @@
 import styles from "./team.module.scss";
-import { Container } from "@design-system/layout/utilities";
 import Image from "next/image";
 import { BranchData } from "@components/views/contact/branch/branch-data";
 import EnvelopeIcon from "@components/shared/icons/envelope.icon";
@@ -9,45 +8,44 @@ function Team(props: { team: BranchData["team"] }) {
   const { team } = props;
 
   return (
-    <Container>
-      <div className={styles.wrapper}>
-        <div className={styles.team}>
-          {team.map((item, index) => (
-            <div key={index} className={styles.member}>
-              <div className={styles.imageWrapper}>
-                <Image src={item.img1} alt={item.name} width={274} height={300} className={styles.image1} />
-                <Image
-                  src={item.img2}
-                  alt={item.name}
-                  width={274}
-                  height={300}
-                  style={{
-                    objectFit: "cover",
-                  }}
-                  className={styles.image2}
-                />
-              </div>
-              <div className={styles.content}>
-                <ul className={styles.list}>
-                  <li className={cx(styles.element, styles.name)}>{item.name}</li>
-                  {item.role ? <li className={styles.element}>{item.role}</li> : null}
-                  {item.phone ? <li className={styles.element}>{item.phone}</li> : null}
-                  <li className={styles.element}>
-                    <div className={styles.iconWrapper}>
-                      <EnvelopeIcon fill={"white"} />
-                    </div>
-                    <span className={styles.emailText}>
-                      {item.email}
-                      <span>@orionexpress.eu</span>
-                    </span>
-                  </li>
-                </ul>
-              </div>
+    <div className={styles.wrapper}>
+      <div className={styles.team}>
+        {team.map((item, index) => (
+          <div key={index} className={styles.member}>
+            <div className={styles.imageWrapper}>
+              <Image quality={100} src={item.img1} alt={item.name} width={274} height={300} className={styles.image1} />
+              <Image
+                src={item.img2}
+                alt={item.name}
+                width={274}
+                height={300}
+                style={{
+                  objectFit: "cover"
+                }}
+                className={styles.image2}
+                quality={100}
+              />
             </div>
-          ))}
-        </div>
+            <div className={styles.content}>
+              <ul className={styles.list}>
+                <li className={cx(styles.element, styles.name)}>{item.name}</li>
+                {item.role ? <li className={styles.element}>{item.role}</li> : null}
+                {item.phone ? <li className={styles.element}>{item.phone}</li> : null}
+                <li className={styles.element}>
+                  <div className={styles.iconWrapper}>
+                    <EnvelopeIcon fill={"white"} />
+                  </div>
+                  <span className={styles.emailText}>
+                      {item.email}
+                    <span>@orionexpress.eu</span>
+                    </span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        ))}
       </div>
-    </Container>
+    </div>
   );
 }
 
