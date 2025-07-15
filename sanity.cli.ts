0f06274acd0167ca08cae0a387525cec48efb162
Michał Kowalski <michalkowit@gmail.com>
2025-07-16 00:39:48 +0200

    Update sanity.cli.ts

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -5,7 +5,7 @@
 
 import { defineCliConfig } from 'sanity/cli'
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+const projectId = "hbpvnf14"
+const dataset = "production"
 
 export default defineCliConfig({ api: { projectId, dataset } })
