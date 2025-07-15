c765a02a3e4857af6fc598bdda9ff8f768a6b2da
Michał Kowalski <michalkowit@gmail.com>
2025-07-16 00:46:43 +0200

    adjusted sanity

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,12 +1,12 @@
 export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2025-07-15";
 
 export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
+  "production",
   "Missing environment variable: NEXT_PUBLIC_SANITY_DATASET",
 );
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  "hbpvnf14",
   "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID",
 );
 
